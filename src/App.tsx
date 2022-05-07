d7a9e3da1069c1730a7346503669a83b47c83c79
michaelbarbas <michaelbarbas@gmail.com>
2022-05-07 15:22:02 -0400

    added auth via web3 and metamask

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import logo from "./adi.png";
+import { Button, Card } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useMoralis } from "react-moralis";
 
 function App() {
+  const { authenticate, isAuthenticated, isAuthenticating, user, account, logout } = useMoralis();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      // add your logic here
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isAuthenticated]);
+
+  const login = async () => {
+    if (!isAuthenticated) {
+
+      await authenticate({signingMessage: "Authenticated via ADI" })
+        .then(function (user) {
+          console.log("Logged in user:", user);
+          console.log(user!.get("ethAddress"));
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container"
+    style={{ height: "auto", position: "relative" }}>
+      <div style={{margin: 0, position: "absolute", top:"50%"}}>
+        <header>
+          <Card.Img className= "container" variant="top" src={logo} />
+        </header>
+        <div className="col-md-12 text-center ">
+          <Button variant="dark" onClick={login}>
+            Connect to Metamask Wallet
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
